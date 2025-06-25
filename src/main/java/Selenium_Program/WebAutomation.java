de2dd37886251f2ee84bdfd01797443e758285eb
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-06-25 11:36:05 +0530

    new repo

---
+++ src/main/java/Selenium_Program/WebAutomation.java
@@ -0,0 +1,29 @@
+package Selenium_Program;
+
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class WebAutomation {
+
+	@Test
+	public void ValidateLoginPage() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.yessinfotech.com/");
+		 
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		if(title.equals("Software training classes institute in Hadapsar Pune")) {
+			System.out.println("TC Passed");
+		}else {
+			System.out.println("TC failed");
+		}
+		
+		driver.close();
+		
+	}
+	
+}
